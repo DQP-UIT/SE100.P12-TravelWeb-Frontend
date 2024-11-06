dad7c94923bda26c0ebcf7477f1dc5d36f0744a5
AkumaP41 <22521094@gm.uit.edu.vn>
2024-11-06 14:33:18 +0700

    Fix conflict

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,27 +3,19 @@ import Default from "../components/default/Default";
 import NotFound from "../pages/notfound/NotFound";
 import Home from "../pages/home/Home";
 import Search from "../pages/search/Search";
-import Default from "../components/default/Default";
-import Home from "../pages/Home/Home";
-import Search from "../pages/search/Search";
-import NotFound from "../pages/notfound/NotFound";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Default/>,
         errorElement: <NotFound/>,
-        element: <Default/>,
-        errorElement: <NotFound/>,
         children: [
             {
                 path: '/',
                 element: <Home/>,
-                element: <Home/>,
             },
             {
                 path: 'search',
-                element: <Search/>,
                 element: <Search/>,
             },
         ]
